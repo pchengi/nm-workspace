ba164fc7a0b2de7c611839c18a3022ac575b7ea9
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2015-12-17 00:51:05 +0100

    fix typo

--- nm-startup.sh
+++ nm-startup.sh
@@ -8,6 +8,6 @@ if [ ! -e /esg/config/esgf.properties ]; then
 	hst=`hostname -f|tr '[a-z]' '[A-Z]'`;
 	quotedhst=`echo $hst|sed 's/[./*?|]/\\\\&/g'`
 	sed -i s/'pcmdi11\.llnl\.gov'/`hostname`/g /esg/config/esgf.properties
-	sed -i s/'PCMDI11\.LLNL\.GOV'/quotedhst/g /esg/config/esgf.properties
+	sed -i s/'PCMDI11\.LLNL\.GOV'/$quotedhst/g /esg/config/esgf.properties
 	echo 0 >/esg/config/config_type
 fi
