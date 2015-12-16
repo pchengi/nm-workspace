8a2379e57391f1e32772e9895443e74eb9f8963b
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2015-12-16 22:53:54 +0100

    add firewall scripts

---
+++ firewall-blocks/block.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo -n >iptables-blocks;
+chmod 600 iptables-blocks;
+run_firewall(){
+	echo -n >iptables-active;
+	chmod 600 iptables-active;
+	cat iptables-boilerplate >>iptables-active
+	cat iptables-blocks >>iptables-active
+	cat iptables-rules >>iptables-active
+	#echo "check iptables-active";
+	bash iptables-active
+	exit 0;
+}
+clear
+while [ 1 ]; do
+	cat <<EOF
+Enter fqdn of node to block, or 
+'all' to block all, or 
+q to quit without blocking any node, or
+s to save and quit with your choices.
+EOF
+	read inp;
+	if [ "$inp" = 'q' -o "$inp" = "all" -o "$inp" = 's' ]; then
+		break;
+	fi
+	if grep -w "$inp" allnodes >/dev/null; then
+		if grep -w "$inp" iptables-blocks >/dev/null; then
+			continue;
+		else
+			echo "iptables -A FILTER -s $inp -j DROP" >>iptables-blocks;
+		fi
+	else
+		continue;
+	fi
+	
+	clear;
+done
+if [ "$inp" = 'q' ]; then
+	echo -n >iptables-blocks;
+	run_firewall
+fi
+if [ "$inp" = "all" ]; then
+	echo -n >iptables-blocks;
+	while read ln; do
+		if [ "$ln" = "$HOSTNAME" ]; then
+			continue;
+		fi
+		echo "iptables -A FILTER -s $ln -j DROP" >>iptables-blocks;
+	done <allnodes
+	run_firewall
+fi
+run_firewall
+
