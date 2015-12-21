7996102c880e7f519377f5c0de73a8529e5ae005
pchengi <pchengi@nsc.liu.se>
2015-12-21 19:44:50 +0100

    cleanup nm-specific files

--- nm-cleanup.sh
+++ nm-cleanup.sh
@@ -11,14 +11,8 @@ if env|grep NO_ESGF >/dev/null; then
 			yum -y remove postgresql-devel postgresql postgresql-server postgresql-libs
 			rm -rf /etc/tempcerts
 			rm -rf /etc/certs
-			rm -f /etc/httpd/conf/nm-httpd.conf
-			rm -f /etc/init.d/nm-httpd
 			rm -rf /opt/esgf
-			rm -f /usr/local/bin/esgf-nm-ctl
 			rm -rf /usr/local/esgf-nodemgr-doc
-			rm -f /esg/log/esgf_nm.log
-			rm -f /esg/log/esgf_nm_dj.log
-			rm -f /esg/log/django.log
 		fi
 	fi
 else
@@ -26,6 +20,12 @@ else
 	rm -rf /esg/tasks
 	rm -f /esg/config/esgf_nodemgr_map.json
 	rm -f /esg/config/nm.properties
+	rm -f /esg/log/esgf_nm.log
+	rm -f /esg/log/esgf_nm_dj.log
+	rm -f /esg/log/django.log
+	rm -f /etc/httpd/conf/nm-httpd.conf
+	rm -f /etc/init.d/nm-httpd
+	rm -f /usr/local/bin/esgf-nm-ctl
 	
 fi
 
