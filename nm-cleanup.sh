c3f9fb4471e54e07bfba2a088757f85fae46ec4f
Prashanth Dwarakanath <pchengi@nsc.liu.se>
2015-12-22 02:45:07 +0100

    fix rewrite rule order

--- nm-cleanup.sh
+++ nm-cleanup.sh
@@ -21,6 +21,7 @@ else
 	echo "There IS ESGF. Tread with care.";
 	service esgf-httpd stop
 	sed -i '/\#nm-http rules start here/,/\#nm-http rules end here/d' /etc/httpd/conf/esgf-httpd.conf
+	sed -i "/esgf-nm/d" /etc/httpd/conf/esgf-httpd.conf
 	service esgf-httpd start
 	rm -f /esg/config/esgf_nodemgr_map.json
 	rm -f /esg/config/nm.properties
