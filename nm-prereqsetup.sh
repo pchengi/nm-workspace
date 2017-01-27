b321f4cc304d4f0f3b326c4ae2f259295b32c967
Sasha Ames <amysash2006@gmail.com>
2017-01-27 13:27:52 -0800

    upgrade django

--- nm-prereqsetup.sh
+++ nm-prereqsetup.sh
@@ -2,7 +2,7 @@
 
 source ./setlocalpaths
 yum -y install postgresql-devel postgresql postgresql-server postgresql-libs mod_ssl
-pip install django==1.8.3
+pip install django==1.10.3
 pip install sqlalchemy
 pip install psycopg2
 pip install requests
